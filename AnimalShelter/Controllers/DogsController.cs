a7bc49c3ebdae58ca856aeb84fa183f232323606
Erika Debelis <erika.debelis@gmail.com>
2021-08-20 12:14:41 -0700

    add get method in all controllers

--- AnimalShelter/Controllers/DogsController.cs
+++ AnimalShelter/Controllers/DogsController.cs
@@ -8,4 +8,40 @@ using AnimalShelter.Models;
 
 namespace AnimalShelter.Controllers
 {
-}
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DogsController : ControllerBase
+  {
+    private readonly AnimalShelterContext _db;
+    public DogsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Dog>>> Get(string name, int age, string description, string gender, string size)
+    {
+      var query = _db.Dogs.AsQueryable();
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+      if (age != 0)
+      {
+        query = query.Where(entry => entry.Age == age);
+      }
+      if (description != null)
+      {
+        query = query.Where(entry => entry.Description == description);
+      }
+      if (gender != null)
+      {
+        query = query.Where(entry => entry.Gender == gender);
+      }
+      if (size != null)
+      {
+        query = query.Where(entry => entry.Size == size);
+      }
+      return await query.ToListAsync();
+    }
+  }  
+}  
--- AnimalShelter/Controllers/SmallAnimals.cs
+++ AnimalShelter/Controllers/SmallAnimals.cs
@@ -8,4 +8,37 @@ using AnimalShelter.Models;
 
 namespace AnimalShelter.Controllers
 {
+  [Route("api/[controller]")]
+  [ApiController]
+  public class SmallAnimalsController : ControllerBase
+  {
+    private readonly AnimalShelterContext _db;
+    public SmallAnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Cat>>> Get(string name, int age, string description, string gender)
+    {
+      var query = _db.Cats.AsQueryable();
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+      if (age != 0)
+      {
+        query = query.Where(entry => entry.Age == age);
+      }
+      if (description != null)
+      {
+        query = query.Where(entry => entry.Description == description);
+      }
+      if (gender != null)
+      {
+        query = query.Where(entry => entry.Gender == gender);
+      }
+      return await query.ToListAsync();
+    }
+  }
 }
